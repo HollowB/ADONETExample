ec94b31107ced0c6fc8b33ae0e322ecf0b61e21e
Gramm <hollow.being@yandex.ru>
2025-05-28 23:42:16 +0300

    Add new method and use some propertys of DbConnection Object

--- DataReader/Program.cs
+++ DataReader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data.Common;
 namespace ADONETExample;
 
 class Program
@@ -9,22 +10,47 @@ class Program
 
         using (SqlConnection connection = new SqlConnection())
         {
-            connection.ConnectionString = "Server=GRAMM\\TESTDB;Database=AutoLot;Trusted_Connection=True;TrustServerCertificate=True;";
+            ShowConnectionInfo(connection);
+            string conString = "Server=GRAMM\\TESTDB;Database=AutoLot;Trusted_Connection=True;TrustServerCertificate=True;Connect Timeout=30;";
+
+            connection.ConnectionString = "Server=GRAMM\\TESTDB;Database=AutoLot;Trusted_Connection=True;TrustServerCertificate=True;Connect Timeout=30;";
+
 
             connection.Open();
+
+            ShowConnectionInfo(connection);
 
             string sql = @"SELECT i.Id, m.Name AS Make, i.Color, i.PetName
                             FROM Inventory i
                             INNER JOIN Makes m on m.Id = i.MakeId ORDER BY i.Id ASC";
             SqlCommand command = new SqlCommand(sql, connection);
+
+
             using (SqlDataReader reader = command.ExecuteReader())
             {
+
                 while (reader.Read())
                 {
-                    Console.WriteLine($"------------\nMake: {reader["Make"]} \nColor: {reader["Color"]} \nPetName: {reader["PetName"]}");
+
+                    Console.WriteLine($"{connection.Database    }");
+                    Console.WriteLine($"------------\nID: {reader["Id"]}\nMake: {reader["Make"]} \nColor: {reader["Color"]} \nPetName: {reader["PetName"]}");
                     Console.WriteLine("------------");
                 }
             }
+
+            
+            connection.Close();
+            ShowConnectionInfo(connection);
+
         }
     }
+
+    static void ShowConnectionInfo (DbConnection connection)
+    {
+        Console.WriteLine($"DB Location: {connection.DataSource}");
+        Console.WriteLine($"DB Name: {connection.Database}");
+        Console.WriteLine($"TimeOut: {connection.ConnectionTimeout}");
+        Console.WriteLine($"Connection state: {connection.State}");
+    }
+
 }
--- DataReader/obj/Debug/net8.0/DataReader.AssemblyInfo.cs
+++ DataReader/obj/Debug/net8.0/DataReader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c586fa7b244477a41b75ab0938a04db912430e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2771fbeb47c44bf656f0d348bc5d38d0a3c30e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
